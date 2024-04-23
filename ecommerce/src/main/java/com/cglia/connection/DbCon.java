78f3de1bb7ebd33e00cb508856b25b5433ecd698
MarniVenkataBabu <129423586+MarniVenkataBabu@users.noreply.github.com>
2024-04-23 12:45:48 +0530

    Update DbCon.java

--- ecommerce/src/main/java/com/cglia/connection/DbCon.java
+++ ecommerce/src/main/java/com/cglia/connection/DbCon.java
@@ -20,7 +20,7 @@ public class DbCon {
 	 public static Connection getConnection() throws ClassNotFoundException, SQLException{
 		 if(connection == null) {
 			 Class.forName("com.mysql.cj.jdbc.Driver");
-			 connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/venkat","root","12345");
+			 connection = DriverManager.getConnection("jdbc:mysql://venkatcart.c9e8mus6uomn.us-east-1.rds.amazonaws.com:3306/venkat","root","7093702308");
 		 }
 		 return connection;
 	 }
